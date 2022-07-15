cf9baa0e55534b0cc0e49137cfec260fcefee913
Denis <denis5918@gmai.com>
2022-07-16 04:06:51 +0700

    refactor

--- app/src/main/java/com/example/messenger/presentation/login/LoginFragment.kt
+++ app/src/main/java/com/example/messenger/presentation/login/LoginFragment.kt
@@ -1,15 +1,19 @@
 package com.example.messenger.presentation.login
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.messenger.R
 import com.example.messenger.presentation.MAIN
+import com.google.android.material.textfield.TextInputLayout
 
 
 class LoginFragment : Fragment() {
@@ -20,6 +24,8 @@ class LoginFragment : Fragment() {
     private lateinit var btnRegister: Button
     private lateinit var etEmail: EditText
     private lateinit var etPassword: EditText
+    private lateinit var tilLogin: TextInputLayout
+    private lateinit var tilPassword: TextInputLayout
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,7 +39,14 @@ class LoginFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         initViews(view)
+        observeViewModel()
+        addTextChangeListeners()
         login()
+        viewModel.userProfile.observe(viewLifecycleOwner){
+            if (it==null){
+                Toast.makeText(activity,"Неверный логин или пароль",Toast.LENGTH_LONG).show()
+            }
+        }
         btnRegister.setOnClickListener {
             MAIN.navController.navigate(R.id.show_register_fragment)
         }
@@ -44,6 +57,58 @@ class LoginFragment : Fragment() {
         btnRegister = view.findViewById(R.id.btn_register)
         etEmail = view.findViewById(R.id.et_email)
         etPassword = view.findViewById(R.id.et_password)
+        tilLogin = view.findViewById(R.id.til_email)
+        tilPassword = view.findViewById(R.id.til_password)
+    }
+
+    private fun observeViewModel() {
+        viewModel.errorInputUserLogin.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.empty_input)
+            } else {
+                null
+            }
+            tilLogin.error = message
+        }
+        viewModel.errorInputPassword.observe(viewLifecycleOwner) {
+            val message = if (it) {
+                getString(R.string.empty_input)
+            } else {
+                null
+            }
+            tilPassword.error = message
+        }
+    }
+
+    private fun addTextChangeListeners() {
+        etEmail.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputLogin()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
+
+
+        etPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                viewModel.resetErrorInputPassword()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+        })
     }
 
     private fun login() {
--- app/src/main/java/com/example/messenger/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/example/messenger/presentation/login/LoginViewModel.kt
@@ -3,8 +3,11 @@ package com.example.messenger.presentation.login
 import android.app.Application
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.example.messenger.data.MessengerRepositoryImpl
+import com.example.messenger.domain.profile.UserProfile
 import com.example.messenger.domain.profile.UserSignIn
 import kotlinx.coroutines.launch
 
@@ -14,25 +17,66 @@ class LoginViewModel(application: Application) : AndroidViewModel(application) {
 
     private val userSignIn = UserSignIn(repository)
 
+    private val _userProfile = MutableLiveData<UserProfile?>()
+    val userProfile: LiveData<UserProfile?>
+        get() = _userProfile
+
+    private val _errorInputPassword = MutableLiveData<Boolean>()
+    val errorInputPassword: LiveData<Boolean>
+        get() = _errorInputPassword
+
+    private val _errorInputUserLogin = MutableLiveData<Boolean>()
+    val errorInputUserLogin: LiveData<Boolean>
+        get() = _errorInputUserLogin
+
     fun signIn(
         inputUserName: String?,
         inputPassword: String?
     ) {
         val userName = parseInput(inputUserName)
         val userPassword = parseInput(inputPassword)
-        viewModelScope.launch {
-            val userProfile = userSignIn.userSignIn(userName, userPassword)
-            if (userProfile == null) {
-                Log.d("LoginViewModel", "такого пользователя не сущ")
-            } else {
-                Log.d("LoginViewModel", userProfile.userFullName)
+        val fieldsValid = validateInput(
+            userName,
+            userPassword
+        )
+        if(fieldsValid) {
+            viewModelScope.launch {
+                val user = userSignIn.userSignIn(userName, userPassword)
+                if (user == null) {
+                    _userProfile.value = null
+                    Log.d("LoginViewModel", "такого пользователя не сущ")
+                } else {
+                    _userProfile.value = user
+                }
             }
         }
-
     }
 
     private fun parseInput(inputString: String?): String {
         return inputString?.trim() ?: ""
     }
 
+    private fun validateInput(
+        userLogin: String,
+        userPassword: String
+    ): Boolean {
+        var result = true
+        if (userLogin.isBlank()) {
+            _errorInputUserLogin.value = true
+            result = false
+        }
+        if (userPassword.isBlank()) {
+            _errorInputPassword.value = true
+            result = false
+        }
+        return result
+    }
+
+    fun resetErrorInputLogin() {
+        _errorInputUserLogin.value = false
+    }
+
+    fun resetErrorInputPassword() {
+        _errorInputPassword.value = false
+    }
 }
